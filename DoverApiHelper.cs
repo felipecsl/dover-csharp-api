b09c54d21b2048896008fde095f5d54af26e2915
Felipe Lima <felipe.lima@gmail.com>
2010-11-18 00:18:30 -0200

    first commit

---
+++ DoverApiHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Diagnostics;
+
+namespace Com.Dover.IntegrationLib {
+	public class DoverApiHelper {
+		internal static string BaseUrl;
+		
+		public DoverApiHelper(string _userName) {
+			string baseAddress = String.Empty;
+
+#if DEBUG
+			baseAddress = "http://api.localdover.com";
+#else
+			baseAddress = "https://api.dovercms.com";
+#endif
+			BaseUrl = String.Format("{0}/{1}", baseAddress, _userName);
+		}
+
+		internal static XDocument MakeRequest(string url) {
+			var meter = new Stopwatch();
+			meter.Start();
+			var document = XDocument.Load(url);
+			meter.Stop();
+
+			Debug.WriteLine("Dover request for " + url + " took " + meter.Elapsed.Seconds + " seconds");
+
+			return document;
+		}
+	}
+}
---
+++ Module.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Com.Dover.IntegrationLib {
+	/// <summary>
+	/// Dover API integration class for manipulating module data
+	/// </summary>
+	public class Module {
+		public Module() {
+		}
+
+		/// <summary>
+		/// Gets the module URL.
+		/// </summary>
+		/// <param name="_moduleId">The module id.</param>
+		/// <returns>The Dover module URL</returns>
+		public string GetModuleUrl(int _moduleId) {
+			return GetApiUrl("module", _moduleId.ToString());
+		}
+
+		private string GetModulesUrl(params int[] _moduleIds) {
+			string ids = String.Empty;
+
+			foreach (int id in _moduleIds) {
+				ids += id + ",";
+			}
+
+			return GetApiUrl("modules", ids.TrimEnd(",".ToCharArray()));
+		}
+
+		private string GetApiUrl(string _operation, string _moduleId) {
+			return String.Format("{0}/{1}/{2}/", DoverApiHelper.BaseUrl, _operation, _moduleId);
+		}
+
+		/// <summary>
+		/// Loads the data xml for the provided module id list.
+		/// </summary>
+		/// <param name="_moduleIds">The module ids.</param>
+		/// <returns>A XML Document with the data from all the specified modules</returns>
+		public XDocument LoadXml(params int[] _moduleIds) {
+			return DoverApiHelper.MakeRequest(GetModulesUrl(_moduleIds));
+		}
+
+		/// <summary>
+		/// Loads the data xml for the provided module id.
+		/// </summary>
+		/// <param name="_moduleId">The module id.</param>
+		/// <returns>>A XML Document with the data from the module</returns>
+		public XDocument LoadXml(int _moduleId) {
+			return DoverApiHelper.MakeRequest(GetModuleUrl(_moduleId));
+		}
+
+		/// <summary>
+		/// Loads the data xml for the specified record in the provided module.
+		/// </summary>
+		/// <param name="_moduleId">The module id.</param>
+		/// <param name="_id">The record id.</param>
+		/// <returns>>A XML Document with the record data from the module</returns>
+		public XDocument LoadXml(int _moduleId, int _id) {
+			return DoverApiHelper.MakeRequest(GetModuleUrl(_moduleId) + _id);
+		}
+
+		/// <summary>
+		/// Loads the data xml for the provided module id, filtered with the provided query string values.
+		/// </summary>
+		/// <param name="_moduleId">The module id.</param>
+		/// <param name="_queryString">The query string for results filtering.</param>
+		/// <returns>A XML Document with the matching records data from the module</returns>
+		public XDocument LoadXml(int _moduleId, string _queryString) {
+			return DoverApiHelper.MakeRequest(GetModuleUrl(_moduleId) + "?" + _queryString);
+		}
+	}
+}
